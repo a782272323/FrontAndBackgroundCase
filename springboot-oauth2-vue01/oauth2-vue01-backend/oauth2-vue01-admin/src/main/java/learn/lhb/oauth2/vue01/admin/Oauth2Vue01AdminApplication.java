33181886ccb2822da85aae580113a540a5571ff1
a782272323 <782272323@qq.com>
2020-07-22 10:50:59 +0800

    测试oauth2验证是否适用于单体，搭建完成多模块开发的基本骨架

---
+++ springboot-oauth2-vue01/oauth2-vue01-backend/oauth2-vue01-admin/src/main/java/learn/lhb/oauth2/vue01/admin/Oauth2Vue01AdminApplication.java
@@ -0,0 +1,13 @@
+package learn.lhb.oauth2.vue01.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(value = "learn.lhb.oauth2.vue01.mapper")
+public class Oauth2Vue01AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Vue01AdminApplication.class, args);
+    }
+}
