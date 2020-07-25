005bfc733378c2d632369c2ae7b50557721a52d4
a782272323 <782272323@qq.com>
2020-07-25 19:06:25 +0800

    测试github+jenkins 持续集成部署 demo01

--- springboot-oauth2-vue01/oauth2-vue01-backend/oauth2-vue01-admin/src/main/java/learn/lhb/oauth2/vue01/admin/controller/TestController.java
+++ springboot-oauth2-vue01/oauth2-vue01-backend/oauth2-vue01-admin/src/main/java/learn/lhb/oauth2/vue01/admin/controller/TestController.java
@@ -47,7 +47,7 @@ public class TestController {
      * 测试缓存的使用
      * @return
      */
-    @GetMapping("redis01")
+    @GetMapping("/test/redis01")
     public BaseResult redisTest01() {
         String key = "201735020325";
         String username = "梁鸿斌";
@@ -58,13 +58,13 @@ public class TestController {
 
         // 先查询缓存是否有数据
         if (redisUtils.hmget(key) != null && redisUtils.hmget(key).size() > 0) {
-            log.info("缓存没有数据");
-            return BaseResult.ok().put(200,"缓存没有数据", "data", redisUtils.hmget(key));
+            log.info("缓存有数据");
+            return BaseResult.ok().put(200,"缓存有数据", "data", redisUtils.hmget(key));
         }
 
         log.info("缓存没有数据，存入缓存");
         if (redisUtils.hmset(key,map,60 * 60 * 12) == true) {
-            return BaseResult.ok("存入缓存");
+            return BaseResult.ok("缓存没有数据,存入缓存");
         }
 
         return BaseResult.error("缓存服务器异常");
